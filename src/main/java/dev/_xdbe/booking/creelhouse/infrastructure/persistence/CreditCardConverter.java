4b9f27a9a2fdab4c8d6193e2ce406bc7ce510571
adam.bokobza <adam.bokobza@epita.fr>
2026-04-15 15:18:46 +0200

    feat: credit card number masking

--- src/main/java/dev/_xdbe/booking/creelhouse/infrastructure/persistence/CreditCardConverter.java
+++ src/main/java/dev/_xdbe/booking/creelhouse/infrastructure/persistence/CreditCardConverter.java
@@ -38,7 +38,7 @@ public class CreditCardConverter implements AttributeConverter<String, String> {
 
     private String panMasking(String pan) {
         // Step 6:
-        return pan;
+        return pan.substring(0, 4) + "*".repeat(pan.length() - 8) + pan.substring(pan.length() - 4);
         // Step 6: End
     }
 
